337d44cc43e8077009f30ec6b5bdf3937881bf1e
marianapetrosink <00001116837912sp@al.educacao.sp.gov.br>
2024-10-24 17:49:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular a área de um triângulo?',
+    'A fórmula para calcular a área de um triângulo é A = (base * altura) / 2.'
+)
+
+criaCartao(
+    'História',
+    'Em que ano ocorreu a Proclamação da República no Brasil?',
+    'A Proclamação da República no Brasil ocorreu em 1889.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o maior continente do mundo?',
+    'O maior continente do mundo é a Ásia.'
+)
+
+criaCartao(
+    'Química',
+    'O que são elementos químicos?',
+    'Elementos químicos são substâncias puras que não podem ser decompostas em outras mais simples e representam diferentes tipos de átomos.'
+)
